3c8c3daed30a304fb904102212b9e413a3cef80d
chuvanloi <loichu1998@gmail.com>
2023-02-26 22:15:34 +0700

    them

--- New folder/index.py
+++ New folder/index.py
@@ -1,42 +1,6 @@
-<?php
-    require_once "config.phpjdhfdfds";
-?>
-
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>QLTB</title>
-</head>
-<body>
-    <table border="1" align="center">
-        <tr>
-            <th style="text-align: center;">Mã Sinh Viên</th>
-            <th style="text-align: center;">HỌ Tên</th>
-            <th style="text-align: center;">Ngày Sinh</th>
-            <th style="text-align: center;">
-                    <a >Action</a>
-            </th>
-        </tr>
-        <?php 
-            $sql = "SELECT *FROM sinhvien";
-            $qr = mysqli_query($link,$sql);
-            while($rows = mysqli_fetch_array($qr)){
-        ?>
-        <tr>
-            <td ><?php echo $rows["masv"]; ?></td>
-            <td ><?php echo $rows["hoten"]; ?></td>
-            <td ><?php echo $rows["ngaysinh"]; ?></td>
-            <td>
-                <a href="create.php">Thêm</a> | 
-                <a href="edit.php?id=<?php echo $rows['masv']; ?>" >Sửa</a> | 
-                <a href="delete.php?id=<?php echo $rows['masv']; ?>" >Xoá</a></td>
-
-        </tr>
-        <?php } ?>
-    </table>
-    
-</body>
-</html>
+import cv2
+img = cv2.imread('anh.jpg')
+edged_img = cv2.Canny(img, 100, 200)
+cv2.imshow('edged image', edged_img)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
